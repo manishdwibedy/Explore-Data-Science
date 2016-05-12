7429d72fe87861a616c476978d64dc3f14240b9b
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-12 16:45:42 -0700

    Commenting code, as the printing to the console is not needed.

--- payoff_challenge/db.py
+++ payoff_challenge/db.py
@@ -14,13 +14,13 @@ cur.execute("""SELECT COUNT(id) from lending_club_2007_2011""")
 
 rows = cur.fetchall()
 
-print 'Number of rows : ' + str(rows[0][0])
+# print 'Number of rows : ' + str(rows[0][0])
 
 cur.execute("""SELECT SUM(funded_amnt) from lending_club_2007_2011""")
 
 rows = cur.fetchall()
 
-print 'Total loan amount : $ ' + str(rows[0][0])
+# print 'Total loan amount : $ ' + str(rows[0][0])
 
 cur.execute("""SELECT *
 FROM lending_club_2007_2011
@@ -28,8 +28,9 @@ LIMIT 1""")
 
 rows = cur.fetchall()
 
-print 'One row data ' + str(rows[0])
+# print 'One row data ' + str(rows[0])
 
+print '---Question 1---\n\n'
 print '\n\nTotal Monthly Loan Amount :'
 cur.execute("""SELECT SUM(funded_amnt),issue_d from lending_club_2007_2011 GROUP BY issue_d ORDER BY issue_d DESC """)
 
@@ -54,6 +55,18 @@ for row in rows:
         print 'Month : ' + month
         print 'Amount $: ' + amount
 
+
+print '\n\nAverage Monthly Loan Amount :'
+cur.execute("""SELECT AVG(funded_amnt),issue_d from lending_club_2007_2011 GROUP BY issue_d ORDER BY issue_d DESC """)
+
+rows = cur.fetchall()
+
+for row in rows:
+    month = str(row[1])
+    amount = str(row[0])
+    if row[0]:
+        print 'Month : ' + month
+        print 'Amount $: ' + amount
 
 print 'Done'
 

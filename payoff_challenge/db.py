4656854ed428d262c9fe26701bbdbe660ca07af3
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-12 17:18:45 -0700

    Removing the username and password from the file.

--- payoff_challenge/db.py
+++ payoff_challenge/db.py
@@ -3,9 +3,6 @@ import psycopg2
 dbname = "intern"
 host = "payoff-showtime.ctranyfsb6o1.us-east-1.rds.amazonaws.com"
 port = 5432
-user = "payoff_intern"
-password = 'reallysecure'
-
 conn = psycopg2.connect(database=dbname, user=user, password=password, host=host, port=port, sslmode='require')
 
 cur = conn.cursor()

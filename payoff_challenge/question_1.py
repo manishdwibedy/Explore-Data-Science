d8fa1134151ce1311de5ff23b7684c0e7d31e800
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-12 17:51:15 -0700

    If the connection is not successful, then exit.

--- payoff_challenge/question_1.py
+++ payoff_challenge/question_1.py
@@ -1,11 +1,18 @@
 import db_connection
+import sys
 
 def get_db_connection():
+    '''
+    Getting the database connection.
+    :return: the database connection object, if connection was successful.
+    '''
     DB = db_connection.DB_Connection()
     connection = DB.getConnection()
 
     if connection:
         return connection
+    else:
+        sys.exit(1)
 
 
 if __name__ == '__main__':

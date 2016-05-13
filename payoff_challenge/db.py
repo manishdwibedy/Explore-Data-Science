0edfb2260bdd4312fc173cf99f2af802cb4b502f
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-12 17:15:56 -0700

    Computing the interest rate as per grade.

--- payoff_challenge/db.py
+++ payoff_challenge/db.py
@@ -76,5 +76,38 @@ rows = cur.fetchall()
 
 print 'Number of grades : ' + str(len(rows))
 
+print '\n\nAverage Rate as per the grade :'
+cur.execute("""SELECT grade, int_rate from lending_club_2007_2011""")
+
+rows = cur.fetchall()
+
+grade_data = []
+for row in rows:
+    grade = str(row[0])
+    interest = str(row[1])
+    if len(interest) > 0:
+        interest = interest.replace('%', '')
+        grade_data.append({
+            'grade': grade,
+            'interest': interest
+        })
+
+grade_distribution = {}
+for grade in grade_data:
+    if grade['grade'] in grade_distribution:
+        grade_info = grade_distribution[grade['grade']]
+        grade_distribution[grade['grade']] = {
+            'sum': grade_info['sum'] + float(grade['interest']) ,
+            'count': grade_info['count'] + 1
+        }
+    else:
+        grade_distribution[grade['grade']] = {
+            'sum': float(grade['interest']),
+            'count': 1
+        }
+
+for grade, grade_info in grade_distribution.iteritems():
+    print 'Grade ' + grade
+    print 'Average Interest : ' + str(grade_info['sum'] / grade_info['count']) + '%'
 print 'Done'
 

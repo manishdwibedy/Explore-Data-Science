66dbf4fb2e098e1fbc53c292c6825b6ab9010126
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-12 19:42:05 -0700

    Getting the monthly total loan amount.

--- payoff_challenge/question_1.py
+++ payoff_challenge/question_1.py
@@ -17,10 +17,38 @@ def get_db_connection():
 
 def question_1(db_connection):
 
+    total_rows = []
     for table in config.tables:
         query = "SELECT SUM(funded_amnt),issue_d from " + table + " GROUP BY issue_d ORDER BY issue_d DESC"
         rows = db.DB().query(db_connection, query)
-        pass
+        total_rows.extend(rows)
+
+    loan_info = {}
+    for loan in total_rows:
+        total_amount = loan[0]
+        date = loan[1]
+
+        date_info = date.split('-')
+        if len(date_info) == 2:
+            month = date_info[0]
+            year = date_info[1]
+
+            if year in loan_info:
+                data = loan_info[year]
+                data.append({
+                    'month': month,
+                    'amount': total_amount
+                })
+            else:
+                data = []
+                data.append({
+                    'month': month,
+                    'amount': total_amount
+                })
+                loan_info[year] = data
+        else:
+            print 'Invalid date'
+    return loan_info
 
 if __name__ == '__main__':
     db_connection = get_db_connection()

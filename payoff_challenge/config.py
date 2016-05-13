9d665ff2e3e730dc127d30ee98d4742162bf058b
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-12 17:49:06 -0700

    Adding the tables.

--- payoff_challenge/config.py
+++ payoff_challenge/config.py
@@ -1,3 +1,5 @@
 dbname = "intern"
 host = "payoff-showtime.ctranyfsb6o1.us-east-1.rds.amazonaws.com"
 port = 5432
+
+tables = ['lending_club_2007_2011', 'lending_club_2012_2013', 'lending_club_2014', 'lending_club_2015']

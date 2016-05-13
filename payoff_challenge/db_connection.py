1579be22f8aee804891a68593142e9a28edf8f92
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-12 17:36:14 -0700

    Creating the DB connection class.

---
+++ payoff_challenge/db_connection.py
@@ -0,0 +1,32 @@
+import os
+import psycopg2
+import config
+
+class DB_Connection(object):
+    def __init__(self):
+        # By default the username and password have not been found
+        self.error = False
+
+    def getInput(self):
+        # Checking if the username is present in the environment variable
+        if 'username' in os.environ:
+            self.user = os.environ.data['username']
+            userNameFound = True
+
+        # Checking if the password is present in the environment variable
+        if 'password' in os.environ:
+            self.password = os.environ['password']
+            passwordFound = True
+
+        # If either username or password was not found, then raise exception
+        if not userNameFound or not passwordFound:
+            self.error = True
+            raise EnvironmentError('Please enter the username and password of the database in the environment variables.')
+
+    def getConnection(self):
+        self.getInput()
+        if not self.error:
+            conn = psycopg2.connect(database=config.dbname, user=self.user, password=self.password, host=config.host, port=config.port, sslmode='require')
+            return conn
+        else:
+            return None
